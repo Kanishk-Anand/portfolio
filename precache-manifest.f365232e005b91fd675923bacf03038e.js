33e4fa4aa38ea42f5719b09492830de9f6d1fcda
Kanishk-Anand <kanishk.kanishk.anand@gmail.com>
2020-01-13 21:42:36 +0530

    Updates

---
+++ precache-manifest.f365232e005b91fd675923bacf03038e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "399362c420f54b972d4262c50db42fdf",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6fda5016a5335d1f159f",
+    "url": "/portfolio/static/css/main.3469fdec.chunk.css"
+  },
+  {
+    "revision": "43e87f37285935021e71",
+    "url": "/portfolio/static/js/2.70ceac7b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.70ceac7b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6fda5016a5335d1f159f",
+    "url": "/portfolio/static/js/main.bec45751.chunk.js"
+  },
+  {
+    "revision": "6fef3673099b4dcdb973",
+    "url": "/portfolio/static/js/runtime-main.fed7eca6.js"
+  }
+]);
