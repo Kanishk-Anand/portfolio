add16008d4991d0da73a6df55bf3abdc304fb628
Kanishk-Anand <kanishk.kanishk.anand@gmail.com>
2020-01-13 22:04:48 +0530

    revert

---
+++ build/precache-manifest.aa984d5b6041ce9ecd7da3e6a0d91caf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6766547328d8627adc25878c293a09ba",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0cb8a0bf91ab8adbffe5",
+    "url": "/portfolio/static/css/main.3469fdec.chunk.css"
+  },
+  {
+    "revision": "43e87f37285935021e71",
+    "url": "/portfolio/static/js/2.70ceac7b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.70ceac7b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0cb8a0bf91ab8adbffe5",
+    "url": "/portfolio/static/js/main.0ec3c2c9.chunk.js"
+  },
+  {
+    "revision": "6fef3673099b4dcdb973",
+    "url": "/portfolio/static/js/runtime-main.fed7eca6.js"
+  }
+]);
